d5cf7bc44a45e4c5be63406e87ecb70efe8195d1
mkstyl3 <crack.mike@gmail.com>
2018-04-20 10:35:27 +0200

    updated User model and added Activity

---
+++ models/activity.js
@@ -0,0 +1,21 @@
+let mongoose = require('mongoose');
+
+// Declaración del esquema
+let ActivitySchema = new mongoose.Schema(
+    {
+        name:          { type: String,   required: true },  // Campo obligatório para insertar
+        latitude:      { type: Number,   required: true },  // Campo obligatório para insertar
+        longitude:     { type: Number,   required: true },  // Campo obligatório para insertar
+        cost:          { type: Number,   required: true },  // Campo obligatório para insertar
+        user1:         { type: String,   required: true },  // Campo obligatório para insertar
+        user2:         { type: String },
+        description:   { type: String },
+        tags:          { type: [String] },
+        date:          { type: Date,     default: Date.now },
+        isDone:        { type: Boolean,  default: false }
+    }
+);
+
+// Exporta el modelo y el esquema a la Base de Datos
+module.exports.schema = ActivitySchema;
+module.exports = mongoose.model('Activity', ActivitySchema);
