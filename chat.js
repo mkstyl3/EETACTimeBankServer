62417abdf7b92f00d4e1e1adb6f07fe15a63347d
Marta <martavivesluis@gmail.com>
2018-05-03 23:09:28 +0200

    finishing function associate id user with socket

--- chat.js
+++ chat.js
@@ -19,61 +19,80 @@ exports.chat = function (io) {
             user = msg.userFrom;
             usersHashMap.set(user, socket);
         });
-
-        socket.on('privateMessage', function (msg) {
-            const frame = JSON.parse(msg);
-            const {message, ...messageee} = frame;
-            const {chatId, ...mens} = frame;
-            Chat.findOne({_id: chatId}, (err, OldChat) => {
-                if (!OldChat) return socket.emit('exception', boom.badData("there's no an id"));
-                if (err) return socket.emit('exception', boom.badImplementation("there is an error"))
-                    ;
+        socket.on('newUser', function (msg) {
+            const id = JSON.parse(msg)
+            User.findOne({_id: id}, (err, myuser) => {
+                if (err) console.log("no user");
                 else {
-                    OldChat.messages.push(message);
-                    OldChat.save(function (err) { // => guardem missatge
+                    myuser.socketId.push(msg)
+                    myuser.save(function (err) { // => guardem missatge
                         if (err) {
-
-                            return socket.emit('exception', boom.badImplementation("there is an error"))
+                            console.log("no s'ha guardat");
                         }
-                        else {
-                            const result = OldChat.users.filter(user => user.userId !== message.userFrom);
-                            if (result) {
-                                User.findOne({_id:result[0].userId}, (err, userComplete) => {
+                        else{
+                        console.log("guardat a la base de dades")}
+                    }
+                    );
+                }
+                ;
 
-                                    if (!userComplete) return socket.emit('exception', boom.badData("there's no an user in db"));
-                                    if (err) return socket.emit('exception', boom.badImplementation("there is an error"))
-                                    else {
-                                        if(userComplete.socketId.length === 0){
-                                            console.log("l'usuari no està connectat");
-                                        }
-                                        else {
-                                            userComplete.socketId.forEach( function(valor) {
-                                                socket.broadcast.to(valor).emit('privateMessage', message);
-                                            });
-                                        }
+            });
 
-                                    }
-                                })
+            socket.on('privateMessage', function (msg) {
+                const frame = JSON.parse(msg);
+                const {message, ...messageee} = frame;
+                const {chatId, ...mens} = frame;
+                Chat.findOne({_id: chatId}, (err, OldChat) => {
+                    if (!OldChat) return socket.emit('exception', boom.badData("there's no an id"));
+                    if (err) return socket.emit('exception', boom.badImplementation("there is an error"))
+                        ;
+                    else {
+                        OldChat.messages.push(message);
+                        OldChat.save(function (err) { // => guardem missatge
+                            if (err) {
 
+                                return socket.emit('exception', boom.badImplementation("there is an error"))
                             }
                             else {
-                                socket.emit('exception', boom.notFound("the other user doesn't exists"))
+                                const result = OldChat.users.filter(user => user.userId !== message.userFrom);
+                                if (result) {
+                                    User.findOne({_id: result[0].userId}, (err, userComplete) => {
+
+                                        if (!userComplete) return socket.emit('exception', boom.badData("there's no an user in db"));
+                                        if (err) return socket.emit('exception', boom.badImplementation("there is an error"))
+                                        else {
+                                            if (userComplete.socketId.length === 0) {
+                                                console.log("l'usuari no està connectat");
+                                            }
+                                            else {
+                                                userComplete.socketId.forEach(function (valor) {
+                                                    socket.broadcast.to(valor).emit('privateMessage', message);
+                                                });
+                                            }
+
+                                        }
+                                    })
+
+                                }
+                                else {
+                                    socket.emit('exception', boom.notFound("the other user doesn't exists"))
+                                }
+
                             }
+                        });
 
-                        }
-                    });
+                        /*var socketReceptor = usersHashMap.get(userTo);
+                        if (socketReceptor != null) {
+                            var response;
+                            response.userTo = userTo;
+                            response.message = message;
+                            response.date = date;
+                            response.readIt = false;
+                            socketReceptor.emit('message', response);
+                        }*/
+                    }
 
-                    /*var socketReceptor = usersHashMap.get(userTo);
-                    if (socketReceptor != null) {
-                        var response;
-                        response.userTo = userTo;
-                        response.message = message;
-                        response.date = date;
-                        response.readIt = false;
-                        socketReceptor.emit('message', response);
-                    }*/
-                }
-
+                });
             });
         });
     });
