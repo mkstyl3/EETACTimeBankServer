25fed1ac158a95ed4d72321ca620ea82268c2001
mkstyl3 <crack.mike@gmail.com>
2018-05-08 09:15:40 +0200

    authorization

--- routes/activities.js
+++ routes/activities.js
@@ -1,11 +1,12 @@
 const router = require('express-promise-router')();
 const activity = require('../controllers/activity');
+const passport = require('passport');
 
 // Funciones sobre Actividades
-router.get('/', activity.selectAllActivities);  // Devuelve una lista con todas las actividades
-router.get('/:id', activity.selectOneActivity); // Devuelve las actividad buscada
-router.post('/', activity.insertActivity);      // Inserta una nueva actividad
-router.put('/:id', activity.updateActivity);    // Actualiza la información de una actividad
-router.delete('/:id', activity.deleteActivity); // Elimina de la Base de Datos la actividad buscada
+router.get('/', passport.authenticate('jwt', { session: false }), activity.selectAllActivities);  // Devuelve una lista con todas las actividades
+router.get('/:id', passport.authenticate('jwt', { session: false }), activity.selectOneActivity); // Devuelve las actividad buscada
+router.post('/', passport.authenticate('jwt', { session: false }), activity.insertActivity);      // Inserta una nueva actividad
+router.put('/:id', passport.authenticate('jwt', { session: false }), activity.updateActivity);    // Actualiza la información de una actividad
+router.delete('/:id', passport.authenticate('jwt', { session: false }), activity.deleteActivity); // Elimina de la Base de Datos la actividad buscada
 
 module.exports = router;
