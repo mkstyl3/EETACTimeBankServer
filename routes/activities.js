2126982e3b6202f951f17d7643c2f90900d2bf19
AnnaDiazPineda <31926227+AnnaDiazPineda@users.noreply.github.com>
2018-05-08 08:50:49 +0200

    ++

--- routes/activities.js
+++ routes/activities.js
@@ -3,6 +3,8 @@ const activity = require('../controllers/activity');
 
 // Funciones sobre Actividades
 router.get('/', activity.selectAllActivities);  // Devuelve una lista con todas las actividades
+router.get('/novetats', activity.populateActivities);
+
 router.get('/:id', activity.selectOneActivity); // Devuelve las actividad buscada
 router.post('/', activity.insertActivity);      // Inserta una nueva actividad
 router.put('/:id', activity.updateActivity);    // Actualiza la información de una actividad
