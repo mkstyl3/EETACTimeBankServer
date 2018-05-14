7ef3f30ab387ae64b60d2b17e981fdf60448ea44
invocamanman <32056120+invocamanman@users.noreply.github.com>
2018-05-14 21:58:08 +0200

    aquet cop si¿? cors

--- app.js
+++ app.js
@@ -47,6 +47,19 @@ app.use(function(req, res, next)
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
     next();
 });
+
+app.use(async (req,res,next) =>
+{
+    /* Allow access from any requesting client */
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    /* Allow access for any of the following Http request types */
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
+
+    /* Set the Http request header */
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
+    next();
+});
 // Mongoose
 MongoDB.connect();
 //chat connections
