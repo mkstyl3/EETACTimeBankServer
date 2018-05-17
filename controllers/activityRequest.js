3630a0fee3a306732d98de687841ad76630187f6
IfJosean <josean7jkami@gmail.com>
2018-05-18 00:47:22 +0200

    wallet updating when an activity is done

--- controllers/activityRequest.js
+++ controllers/activityRequest.js
@@ -8,12 +8,12 @@ const mongoosePaginate = require('mongoose-pagination');
 
 // Devuelve una peticio
 exports.getRequest = function (req, res) {
-    let requestId = req.params.id
-    console.log(requestId)
+    let requestId = req.params.id;
+    console.log(requestId);
 
     ActivityRequest.findById(requestId, (err, request)=> {
-        if(err)     return res.status(500).send({message: `error al realitzar la petició: ${err}`})
-        if(request) return res.status(404).send({message: `la peticio no existeix: ${err}`})
+        if(err)     return res.status(500).send({message: `error al realitzar la petició: ${err}`});
+        if(request) return res.status(404).send({message: `la peticio no existeix: ${err}`});
 
         res.status(200).send({request: request})
     })
@@ -26,7 +26,7 @@ exports.getRequests = function (req, res) {
             console.log(err);
             return res.status(500).send({message:`error al realizar la petición: ${err} `})
         }
-        if(!requests)   return res.status(404).send({message: `el array de peticions no existe: ${err}`})
+        if(!requests)   return res.status(404).send({message: `el array de peticions no existe: ${err}`});
 
         res.status(200).send(requests)
 
@@ -35,22 +35,22 @@ exports.getRequests = function (req, res) {
 
 // Inserta una nova peticio
 exports.insertActivityRequest = function (req, res) {
-    console.log(req.bodyParser)
+    console.log(req.bodyParser);
     //afegint a la BD
-    let activityRequest = new ActivityRequest()
-        activityRequest.userFrom    = req.body.userFrom
-        activityRequest.userTo      = req.body.userTo
-        activityRequest.activity      = req.body.activity
+    let activityRequest = new ActivityRequest();
+        activityRequest.userFrom    = req.body.userFrom;
+        activityRequest.userTo      = req.body.userTo;
+        activityRequest.activity      = req.body.activity;
 
 
     activityRequest.save((err, requestStored) => {
-        if(err) res.status(200).send({message:`error en salvar la BD: ${err}`})
+        if(err) res.status(200).send({message:`error en salvar la BD: ${err}`});
         res.status(200).send({activityRequest: requestStored})
     })
 };
 
 exports.insertActivityRequestFromName = async function (req, res) {
-    console.log(req.bodyParser)
+    console.log(req.bodyParser);
     let userFrom = await User.findOne({'username':req.body.userFrom});
     let userTo = await User.findOne({'username':req.body.userTo});
 
@@ -60,37 +60,37 @@ exports.insertActivityRequestFromName = async function (req, res) {
         return;
     }
 
-    let activityRequest = new ActivityRequest()
+    let activityRequest = new ActivityRequest();
         activityRequest.userFrom    = userFrom.id;
         activityRequest.userTo      = userTo.id;
         activityRequest.activity      = req.body.activity;
-    
-    
+
+
     activityRequest.save((err, requestStored) => {
-        if(err) res.status(200).send({message:`error en salvar la BD: ${err}`})
+        if(err) res.status(200).send({message:`error en salvar la BD: ${err}`});
         res.status(200).send({activityRequest: requestStored})
     })
 };
 
 // Actualiza la información de una petició
 exports.updateRequest = function (req, res) {
-    let requestId = req.params.requestId
-    let update    = req.body
+    let requestId = req.params.requestId;
+    let update    = req.body;
 
     activityRequest.findByidandUpdate( requestId, update, (err, requestUpdated) => {
-        if(err)  return res.status(500).send({message:`error al actualitzar la peticio ${err} `})
+        if(err)  return res.status(500).send({message:`error al actualitzar la peticio ${err} `});
         res.status(200).send({tag: requestUpdated})
     })
 };
 
 // Elimina una petició
 exports.deleteActivityRequest = function (req, res) {
-    let requestId = req.params.id
+    let requestId = req.params.id;
     ActivityRequest.findById(requestId, (err, request) =>{
-        if (err)    res.status(500).send({message:`error al realizar la petición: ${err} `})
+        if (err)    res.status(500).send({message:`error al realizar la petición: ${err} `});
 
         request.remove(err => {
-            if(err) res.status(500).send({message:`error al borrar la petició: ${err} `})
+            if(err) res.status(500).send({message:`error al borrar la petició: ${err} `});
 
             res.status(200).send({message: `peticio eliminada` })
 
@@ -101,7 +101,7 @@ exports.deleteActivityRequest = function (req, res) {
 
 //llistat paginat dels usuaris que tenen una peticio meva
 exports.getRequestsPag = function(req, res){
-    let userId = req.params.id
+    let userId = req.params.id;
     ActivityRequest.find({userFrom: userId})
                    .populate({path: 'activity'})
                    .populate({path: 'userTo'})
@@ -115,7 +115,7 @@ exports.getRequestsPag = function(req, res){
 
 //llista paginada dels usuaris que m'han fet una peticio
 exports.getPetitions = function (req, res) {
-    let userId = req.params.id
+    let userId = req.params.id;
 
     ActivityRequest.find({userTo: userId})
                    .populate('userFrom')
@@ -127,7 +127,7 @@ exports.getPetitions = function (req, res) {
 
                        res.status(200).send(req2);
     })
-    
+
 };
 
 exports.acceptRequest = async function (req,res){
@@ -141,24 +141,35 @@ exports.acceptRequest = async function (req,res){
     else{
         res.status(404).send({'message':'peticion no encontrada'});
     }
-} 
+};
 
 exports.doneRequest = async function (req,res){
     console.log(req.body);
     let request = await ActivityRequest.findOne({'_id':req.body.id});
     if(request){
-        request.isDone = true;
-        request.save();
-        res.status(200).send({'message':'ok'});
+      request.isDone = true;
+      request.save();
+
+      let activity = await Activity.findOne({'_id':request.activity});
+      let userFrom = await User.findOne({'_id':request.userFrom});
+      userFrom.wallet = userFrom.wallet - activity.cost;
+
+      let userTo = await User.findOne({'_id':request.userTo});
+      userTo.wallet = userTo.wallet + activity.cost;
+
+      userFrom.save();
+      userTo.save();
+
+      res.status(200).send({'message':'ok'});
     }
     else{
         res.status(404).send({'message':'peticion no encontrada'});
     }
-} 
+};
 
 //contadors
 exports.getCounters = function (req, res) {
-    let id = req.params.id
+    let id = req.params.id;
     getCountPetitions(id).then((value)=>{
         return res.status(200).send(value);
         //console.log(value);
@@ -166,7 +177,7 @@ exports.getCounters = function (req, res) {
         .catch((err) => {
             return 'error occured'
     });
-}
+};
     async function getCountPetitions(id) {
 
 
@@ -193,3 +204,4 @@ exports.getCounters = function (req, res) {
                     requested: requested, petitions: petitions
                 }
     }
+
